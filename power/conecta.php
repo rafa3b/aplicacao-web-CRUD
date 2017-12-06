a8768206e3000c7d4baf3ce0ca0419d4d7cca8a6
Rafael <rafa3b@gmail.com>
2017-12-06 10:51:51 -0200

    adicionado arquivo de conexao ao banco de dados

---
+++ power/conecta.php
@@ -0,0 +1,17 @@
+﻿<?php 
+$servername = "servername";
+$username = "username";
+$password = "password";
+$dbname = "dbname";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+$conn->query("SET NAMES 'utf8'");
+$conn->query('SET character_set_connection=utf8');
+$conn->query('SET character_set_client=utf8');
+$conn->query('SET character_set_results=utf8');
+
+?>
